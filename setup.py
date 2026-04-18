a2fd18a49ef71d07227eab674a16729e01804855
shivani-14-dixit <shivanidixit84@gmail.com>
2026-04-18 17:16:58 +0530

    setup and req

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name= 'ml_proj',
+    version= '0.0.1',
+    author= 'Shivani',
+    author_email='shivanidixit84@gmail.com',
+    packages='find_packages',
+    
+)
